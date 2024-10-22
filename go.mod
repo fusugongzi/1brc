c08aaded3aed485a29a31b02cc81a773beeb5268
wangzhenzhong <15600804671@163.com>
2024-10-22 10:47:54 +0800

    feat: function done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module brc
 
 go 1.19
+
+require golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
--- main.go
+++ main.go
@@ -1,85 +1,160 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"runtime"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
-type city struct {
-	name string
-	m    float64
+type measurement struct {
+	max float64
+	min float64
+	sum float64
+	cnt int64
+	avg float64
 }
 
-type measurement struct {
+type computedResult struct {
 	name string
 	max  float64
 	min  float64
-	sum  float64
-	cnt  int64
 	avg  float64
 }
 
 func main() {
 	start := time.Now().UnixMilli()
 
-	readFileStart := time.Now().UnixMilli()
-	bytes, err := os.ReadFile("measurements.txt")
-	if err != nil {
-		fmt.Println("read file error")
-	}
-	fmt.Println("read file spend: " + strconv.FormatInt(time.Now().UnixMilli()-readFileStart, 10) + " ms")
+	bytesChan := make(chan []byte, 15)
 
-	cities := make([]city, 0)
-	analyStart := time.Now().UnixMilli()
-	for _, v := range strings.Split(string(bytes), "\n") {
-		vs := strings.Split(v, ";")
-		m, _ := strconv.ParseFloat(vs[1], 64)
-		cities = append(cities, city{
-			name: vs[0],
-			m:    m,
-		})
+	// 使用协程处理数据
+	wg := sync.WaitGroup{}
+	wg.Add(runtime.NumCPU() - 1)
+	dataChan := make(chan map[string]*measurement, 10)
+	for i := 0; i < runtime.NumCPU()-1; i++ {
+		go func() {
+			for readBytes := range bytesChan {
+				process(readBytes, dataChan)
+			}
+			wg.Done()
+		}()
 	}
-	fmt.Println("analy spend: " + strconv.FormatInt(time.Now().UnixMilli()-analyStart, 10) + " ms")
 
-	calStart := time.Now().UnixMilli()
-	measurementMap := make(map[string]*measurement)
-	for _, v := range cities {
-		if exist, ok := measurementMap[v.name]; !ok {
-			measurementMap[v.name] = &measurement{
-				name: v.name,
-				min:  v.m,
-				max:  v.m,
-				sum:  v.m,
-				cnt:  1,
+	// 读取文件，写入到bytesChan
+	go func() {
+		// 打开文件
+		file, err := os.Open("measurements.txt")
+		if err != nil {
+			fmt.Println("打开文件错误:", err)
+			return
+		}
+
+		// 创建带缓冲的读取器，增大缓冲区大小
+		reader := bufio.NewReaderSize(file, 500*1024*1024) // 16 KB
+
+		sendBytes := make([]byte, 0)
+		leaveBytes := make([]byte, 0)
+		// 逐行读取文件
+		for {
+			readBytes := make([]byte, 64*1024*1024)
+			_, err = reader.Read(readBytes)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				panic(err)
 			}
-		} else {
-			if v.m < exist.min {
-				exist.min = v.m
-			} else if v.m > exist.max {
-				exist.max = v.m
+			lastNewLineIdx := bytes.LastIndexByte(readBytes, byte('\n'))
+			sendBytes = append(sendBytes, leaveBytes...)
+			sendBytes = append(sendBytes, readBytes[:lastNewLineIdx+1]...)
+			leaveBytes = make([]byte, len(readBytes[lastNewLineIdx+1:]))
+			copy(leaveBytes, readBytes[lastNewLineIdx+1:])
+			bytesChan <- sendBytes
+		}
+
+		close(bytesChan)
+
+		// 协程处理完数据之后，关闭dataChan
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	// 汇总最终结果
+	result := make(map[string]*measurement)
+	for v := range dataChan {
+		for c, measure := range v {
+			c := c
+			measure := measure
+			if result[c] == nil {
+				result[c] = measure
+			} else {
+				exist := result[c]
+				if measure.min < exist.min {
+					exist.min = measure.min
+				}
+				if measure.max > exist.max {
+					exist.max = measure.max
+				}
+				exist.sum = exist.sum + measure.sum
+				exist.cnt = exist.cnt + measure.cnt
 			}
-			exist.sum = exist.sum + v.m
-			exist.cnt++
 		}
 	}
-	fmt.Println("cal spend: " + strconv.FormatInt(time.Now().UnixMilli()-calStart, 10) + " ms")
 
-	measurements := make([]*measurement, 0)
-	for _, v := range measurementMap {
-		v := v
-		v.avg = v.sum / float64(v.cnt)
-		measurements = append(measurements, v)
+	// 输出最终结果
+	computedResults := make([]*computedResult, 0)
+	for k, v := range result {
+		computedResults = append(computedResults, &computedResult{
+			name: k,
+			max:  v.max,
+			min:  v.min,
+			avg:  float64(v.sum) / float64(v.cnt),
+		})
 	}
-	sort.SliceStable(measurements, func(i, j int) bool {
-		return measurements[i].name < measurements[j].name
+	sort.SliceStable(computedResults, func(i, j int) bool {
+		return computedResults[i].name < computedResults[j].name
 	})
-	for _, v := range measurements {
+	for _, v := range computedResults {
 		fmt.Println(fmt.Sprintf("%s/%.2f/%.2f/%.2f", v.name, v.min, v.avg, v.max))
 	}
 
-	fmt.Println("total spend: " + strconv.FormatInt(time.Now().UnixMilli()-start, 10) + " ms")
+	fmt.Println("spend time: " + strconv.FormatInt(time.Now().UnixMilli()-start, 10) + " ms")
+}
+
+func process(readBytes []byte, dataChan chan map[string]*measurement) {
+	m := make(map[string]*measurement)
+	str := string(readBytes)
+	strSlice := strings.Split(str, "\n")
+	for _, v := range strSlice {
+		if v == "" {
+			continue
+		}
+		c := strings.Split(v, ";")[0]
+		measure, _ := strconv.ParseFloat(strings.Split(v, ";")[1], 64)
+		if exist, ok := m[c]; !ok {
+			m[c] = &measurement{
+				min: measure,
+				max: measure,
+				sum: measure,
+				cnt: 1,
+			}
+		} else {
+			if measure < exist.min {
+				exist.min = measure
+			} else if measure > exist.max {
+				exist.max = measure
+			}
+			exist.sum = exist.sum + measure
+			exist.cnt++
+		}
+	}
+	dataChan <- m
 }
